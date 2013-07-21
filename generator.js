e6766d647592f040531543db0a8f6faf9c9f98e2
Hack Reactor Students <github@hackreactor.com>
2013-07-20 17:33:12 -0700

    New entries!

--- generator.js
+++ generator.js
@@ -1,17 +1,19 @@
-var prefix = ["twit", "mack", "boosh", "whyr", "bongo", "wham", "krak", "mini", "async", "hoob", "droom", "what", "onion", "spork", "whiz", "knit", "whet", "ramp", "flit", "whirl", "barz"];
-var suffix = [".io", ".ly", ".me", "ify", ".geocities.com", ".com", ".net", ".biz", ".info", "urbate", "ster", "book"];
-var company = ["Facebook", "Reddit", "Twitter", "Yelp", "Instagram", "Fandango", "eBay", "Amazon", "Spotify", "Tumblr", "Netflix", "Pinterest", "Foursquare", "StubHub", "GitHub", "Wikipedia", "LinkedIn", "Chatroulette", "a 1990s Lycos chatroom", "Google Reader", "OKCupid", "Craigslist", "Coursera", "Grindr", "HackerNews"];
-var noun = ["homeless people", "hookups", "Justin Bieber fans", "cougars", "hipsters", "yoga", "single dads", "dogs", "stoners", "overachievers", "cops", "bored housewives", "the obese", "Baby Boomers", "Republicans", "progressives", "the LGBT community", "Kennedy assassination conspiracy theorists", "fans of The West Wing", "the mining industry", "CEOs", "trees"];
+
+var prefix = ["twit", "mack", "boosh", "whyr", "bongo", "wham", "krak", "min", "async", "hoob", "droom", "what", "ion", "spork", "whiz", "knit", "whet", "laff", "ramp", "flit", "whirl", "barz", "ball", "iron", "shop"];
+var suffix = [".io", ".ly", ".me", "ify", ".geocities.com", ".com", ".net", ".biz", "urbate", "ster", "book", "la", "olator", "adoo", "hub", "erest", "r", "oo", "assa", "er", "istry", "head", "ista"];
+var company = ["Facebook", "Reddit", "Twitter", "Yelp", "Instagram", "Fandango", "eBay", "Amazon", "Spotify", "Tumblr", "a meme generator", "Netflix", "Wordpress", "Pinterest", "Groupon", "Flickr", "LiveJournal", "Hot Or Not", "Pets.com", "Angry Birds", "Foursquare", "StubHub", "GitHub", "MySpace", "SoundCloud", "Pitchfork", "Buzzfeed", "Wikipedia", "LinkedIn", "Chatroulette", "a 1990s Lycos chatroom", "Google Reader", "OKCupid", "Craigslist", "Coursera", "Grindr", "HackerNews"];
+var customer = ["Justin Bieber fans", "cougars", "hipsters", "yoga", "single dads", "musicians", "dogs", "homeless people", "hookups", "stoners", "overachievers", "cops", "soccer moms", "Millennials", "urban professionals", "Baby Boomers", "Republicans", "preppies", "flashmobs", "DJs", "veterans", "the elderly", "progressives", "college students", "newlyweds", "the LGBT community", "investment bankers", "evangelicals", "freelance writers", "Kennedy assassination conspiracy theorists", "movie lovers", "foodies", "fans of The West Wing", "the mining industry", "CEOs", "trees"];
+
+var generate = function(){
+  $('#prefix').html(prefix[Math.floor(Math.random()*prefix.length)]);
+  $('#suffix').html(suffix[Math.floor(Math.random()*suffix.length)]);
+  $('#company').html(company[Math.floor(Math.random()*company.length)]);
+  $('#customer').html(customer[Math.floor(Math.random()*customer.length)]);
+};
 
 $(document).ready(function() {
-    $('#prefix').html(prefix[Math.floor(Math.random()*prefix.length)]);
-    $('#suffix').html(suffix[Math.floor(Math.random()*suffix.length)]);
-    $('#company').html(company[Math.floor(Math.random()*company.length)]);
-    $('#noun').html(noun[Math.floor(Math.random()*noun.length)]);
+  generate();
   $('#button').click(function(){
-    $('#prefix').html(prefix[Math.floor(Math.random()*prefix.length)]);
-    $('#suffix').html(suffix[Math.floor(Math.random()*suffix.length)]);
-    $('#company').html(company[Math.floor(Math.random()*company.length)]);
-    $('#noun').html(noun[Math.floor(Math.random()*noun.length)]);
-  })
+    generate();
+  });
 });
